ccf83ee8a9b0bc594942e17a6cbbc18b1e3784c0
Christopher Luvumbo <christopherluvumbo@gmail.com>
2022-02-01 16:40:57 +0100

    working with props

--- src/index.js
+++ src/index.js
@@ -2,47 +2,54 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+const title = 'Atomic Habits';
+const author = 'James Clear';
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/515mvgd0pXL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg';
+
 const BookList = () => {
   return (
     <section className='bookList'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book title='ramdon title' job='developper' />
       <Book />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className='book'>
-      <Title />
-      <Author />
-      <Image />
+      <h2>{title.toUpperCase()}</h2>
+      <p>{author}</p>
+      <img src={img} alt='Book cover' />
     </article>
   );
 };
 
-const Title = () => {
-  return <h2>Atomic Habits</h2>;
-};
+/* Ejemplo de uso de las props */
 
-const Author = () => {
-  return <p style={{ margin: '0.3rem auto', color: 'blue' }}>James Clear</p>;
-};
-
-const Image = () => {
+const ListOfPeople = () => {
   return (
-    <img
-      src='https://images-na.ssl-images-amazon.com/images/I/515mvgd0pXL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
-      alt='Book cover'
-    />
+    <section className='bookList'>
+      <Person name='Christopher' lastName='Luvumbo' age={25} />
+    </section>
   );
 };
 
-ReactDom.render(<BookList />, document.getElementById('root'));
+const Person = (props) => {
+  return (
+    <>
+      <p>{props.name}</p>
+      <p>{props.lastName}</p>
+      <p>{props.age}</p>
+    </>
+  );
+};
+
+ReactDom.render(
+  <>
+    <BookList />
+    <ListOfPeople />
+  </>,
+  document.getElementById('root')
+);
