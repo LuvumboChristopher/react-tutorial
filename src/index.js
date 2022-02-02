39de747c16c043b72d60641d5c2cd26e3261ba2c
Christopher Luvumbo <christopherluvumbo@gmail.com>
2022-02-02 08:52:05 +0100

    Using events in Js

--- src/index.js
+++ src/index.js
@@ -2,73 +2,57 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-const firstBook = {
-  title: 'Atomic Habits',
-  author: 'James Clear',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/515mvgd0pXL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
-};
+const books = [
+  {
+    id: 1,
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/515mvgd0pXL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+    description:
+      ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,maxime. Culpa enim saepe quis debitis itaque nulla vel blanditiis  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,maxime. Culpa enim saepe quis debitis itaque nulla vel blanditiis',
+  },
 
-const secondBook = {
-  title: 'Los 4 acuerdos',
-  author: 'Don Miguel Ruiz',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51QAKMAGrJL._SX343_BO1,204,203,200_.jpg',
-};
+  {
+    id: 2,
+    title: 'Los 4 acuerdos',
+    author: 'Don Miguel Ruiz',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51QAKMAGrJL._SX343_BO1,204,203,200_.jpg',
+    description:
+      ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,maxime. Culpa enim saepe quis debitis itaque nulla vel blanditiis  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,maxime. Culpa enim saepe quis debitis itaque nulla vel blanditiis',
+  },
 
-const thirdBook = {
-  title: 'Las 48 leyes del poder',
-  author: 'Robert Green',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/41KY-NORo9L._SX355_BO1,204,203,200_.jpg',
-};
+  {
+    id: 3,
+    title: 'Las 48 leyes del poder',
+    author: 'Robert Green',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/41KY-NORo9L._SX355_BO1,204,203,200_.jpg',
+    description:
+      ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,maxime. Culpa enim saepe quis debitis itaque nulla vel blanditiis  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,maxime. Culpa enim saepe quis debitis itaque nulla vel blanditiis',
+  },
+];
 
 const BookList = () => {
   return (
     <section className='bookList'>
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
-          maxime. Culpa enim saepe quis debitis itaque nulla vel blanditiis
-          architecto.
-        </p>
-      </Book>
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
-          maxime. Culpa enim saepe quis debitis itaque nulla vel blanditiis
-          architecto.
-        </p>
-      </Book>
-      <Book
-        title={thirdBook.title}
-        author={thirdBook.author}
-        img={thirdBook.img}
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
-          maxime. Culpa enim saepe quis debitis itaque nulla vel blanditiis
-          architecto.
-        </p>
-      </Book>
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { title, author, img, children } = props;
-
+  const { title, author, img, description } = props;
+  const clickHandlers = ()=>{
+    alert('Hello world')
+  }
   return (
     <article className='book'>
       <h2>{title}</h2>
-      <p>{author}</p>
+      <h4>{author}</h4>
       <img src={img} alt='Book cover' />
-      <p>{children}</p>
+      <p>{description}</p>
+      <button onClick={clickHandlers}>View more</button>
     </article>
   );
 };
